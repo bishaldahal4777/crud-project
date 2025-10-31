30c207c5b99db6799049a3e544b2699a5eb0edf7
Bishal Dahal <bishaldahal4777@gmail.com>
2025-10-31 06:59:28 +0545

    fix db

--- enroll/models.py
+++ enroll/models.py
@@ -1,12 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
 class Employee(models.Model):
-    emp_id = models.IntegerField(max_length=4)
+    emp_id = models.IntegerField()
     emp_name = models.CharField(max_length=100)
     emp_dept = models.CharField(max_length=100)
-    created_at = models.DateTimeField(auto_now_add=True)
+
 
     def __str__(self):
-        return f"self.emp_name - self.emp-dept"
-
+        return f"{self.emp_name} - {self.emp_dept}"
