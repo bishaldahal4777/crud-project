273cbc6d0c88ccf15f1c665766ee7bf768a7029d
Bishal Dahal <bishaldahal4777@gmail.com>
2025-10-31 06:59:45 +0545

    views

--- enroll/views.py
+++ enroll/views.py
@@ -1,3 +1,6 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import Employee
 # Create your views here.
+def home(request):
+    Employees = Employee.objects.all()
+    
