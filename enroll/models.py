a5ae4b10f76e75cb592e62f5feab5661d3c60dba
Bishal Dahal <bishaldahal4777@gmail.com>
2025-10-31 06:51:08 +0545

    models

--- enroll/models.py
+++ enroll/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Employee(models.Model):
+    emp_id = models.IntegerField(max_length=4)
+    emp_name = models.CharField(max_length=100)
+    emp_dept = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f"self.emp_name - self.emp-dept"
+
