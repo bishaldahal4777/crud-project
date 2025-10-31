2f4c75e7dc7f4d4494f96006fae7340e5196a3de
Bishal Dahal <bishaldahal4777@gmail.com>
2025-10-31 06:55:10 +0545

    fix db

--- enroll/models.py
+++ enroll/models.py
@@ -5,6 +5,7 @@ class Employee(models.Model):
     emp_id = models.IntegerField(max_length=4)
     emp_name = models.CharField(max_length=100)
     emp_dept = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return f"self.emp_name - self.emp-dept"
